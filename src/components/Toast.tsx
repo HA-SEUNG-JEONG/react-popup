0f9a121000859357a626dd5ebbb5ead830b55abf
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-05-18 22:02:28 +0900

    Feat : Toast 스타일 및 error 타입 변경 - error 여부에 따라 background-color 동적으로 변경되도록 수정 -  error type 변경 : boolean 👉 string

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,21 +1,25 @@
-import styled, { css, keyframes } from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Toast = ({ show, isError }: { show: boolean; isError: boolean }) => {
+interface ErrorProps {
+  isError: boolean;
+}
+
+const Toast = ({ isError }: ErrorProps) => {
   return (
-    <Notification show={show} isError={isError}>
-      <p>hi</p>
+    <Notification error={isError ? "true" : "false"}>
+      <p>{isError ? "Fail" : "Success"}</p>
       <Progress></Progress>
     </Notification>
   );
 };
 
 const FadeIn = keyframes`
-  10%{
+  from{
     opacity:1;
     visibility:visible;
     transform:translateY(0);
   }
-  90%{
+  to{
     opacity:1;
     transform:translateY(0);
   }
@@ -27,44 +31,43 @@ const Load = keyframes`
   }
 `;
 
-const Notification = styled.section<{ show: boolean; isError: boolean }>`
+const Notification = styled.section<{ error: string }>`
   position: absolute;
-  top: 10px;
+  top: 1.25rem;
+  left: 50%;
   display: flex;
   justify-content: center;
-  width: 30%;
-  padding: 5px 5px;
+  align-items: center;
+  width: 18rem;
+  height: 3rem;
+  margin-left: -9rem;
+  padding: 0 0.3rem;
   border-radius: 4px;
   background-color: #141619;
   color: #f6f5f9;
-  box-shadow: 0 1px 10px rgba(0, 0, 0, 0.1);
   transform: translateY(30px);
   opacity: 0;
   visibility: hidden;
   animation: ${FadeIn} 4s linear forwards;
-  ${(show) =>
-    show &&
-    css`
-      background-color: green;
-    `};
-
   p {
     font-size: 20px;
     font-weight: 900;
   }
+  background-color: ${({ error }) =>
+    error === "true" ? "#E74C3C" : "rgb(7, 188, 12)"};
 `;
 
 const Progress = styled.span`
   position: absolute;
-  left: 5px;
-  bottom: 5px;
+  left: 0.3rem;
+  bottom: 0.3rem;
   width: calc(100% - 10px);
   height: 3px;
   transform: scaleX(0);
   transform-origin: left;
   background-image: linear-gradient(to right, #539bdb, #3250bf);
   border-radius: inherit;
-  animation: ${Load} 3s 0.25s linear forwards;
+  animation: ${Load} 4s 0.25s linear forwards;
 `;
 
 export default Toast;
